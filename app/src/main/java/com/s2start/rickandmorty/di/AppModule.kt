a944907c40e4fa026fe207953f45831cc87fccd9
olucas <mateuslucsa.ti@gmail.com>
2024-11-04 22:01:18 -0300

    - estudo github

--- app/src/main/java/com/s2start/rickandmorty/di/AppModule.kt
+++ app/src/main/java/com/s2start/rickandmorty/di/AppModule.kt
@@ -6,7 +6,7 @@ import com.s2start.rickandmorty.ui.character.CharacterModel
 import com.s2start.rickandmorty.ui.character.ListCharacterViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-
+//testando git hub
 val appModule = module {
     single<List<CharacterModel>> { listCharacters }
 
